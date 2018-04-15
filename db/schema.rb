c2927cd59d195a15799eafcbf23a5c1c545ccf73
Taff Gao <gaotongfei1995@gmail.com>
2018-04-15 15:01:45 +0800

    setup rolify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415064826) do
+ActiveRecord::Schema.define(version: 20180415065957) do
 
   create_table "agencies", force: :cascade do |t|
     t.string "name"
@@ -119,6 +119,17 @@ ActiveRecord::Schema.define(version: 20180415064826) do
     t.index ["room_id"], name: "index_reviews_on_room_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "home_type"
     t.string "room_type"
@@ -188,6 +199,14 @@ ActiveRecord::Schema.define(version: 20180415064826) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
