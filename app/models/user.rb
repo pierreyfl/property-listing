0680822d7b7c641d04b485674cfbcfcd1f6c3544
Arash Naveed <arsh.nvd@gmail.com>
2018-05-22 15:30:47 +0500

    Fix syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook], :confirmable 
+         :recoverable, :rememberable, :trackable, :confirmable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
   validates :fullname, presence: true, length: { maximum: 50 }
   has_many :rooms
