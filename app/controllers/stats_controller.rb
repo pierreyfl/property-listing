1bd3204ca3dd60cdb9b9c838c507cb99e7d86d73
Abubakr Abd El Hafiz <abubakr35@hotmail.com>
2018-05-22 05:13:57 +0200

    create stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 class StatsController < AdminController
 
   def index
-    @stats = RoomVisit.joins(:room).select('rooms.id, room_visits.user_id, room_visits.email, sum(room_visits.time_spent) as time_spent').group(:room_id, :user_id)
+    @stats = RoomVisit.joins(:room).select('rooms.id, room_visits.user_id, room_visits.email, sum(room_visits.time_spent) as time_spent').group('rooms.id', 'room_visits.user_id', 'room_visits.email')
 
   end
 end
