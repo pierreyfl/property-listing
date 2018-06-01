324e0a5fa0d0cfee0fa4cf66f1770534835ede14
Arash Naveed <arsh.nvd@gmail.com>
2018-06-01 16:43:20 +0500

    Add beds, baths and parking filters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     query = params[:q].presence || '*'
 
     @conditions = {}
-    filters = ["type", "price", "parking", "bathrooms"]
+    filters = ["type", "price", "parking", "bathrooms", "bedrooms"]
 
     params.keys.each do |key|
       method(key.to_sym).call if filters.include?(key)
@@ -25,6 +25,24 @@ class PagesController < ApplicationController
       min = params[:price][:min].to_i
       max = params[:price][:max].blank? ? (1.0 / 0.0) : params[:price][:max].to_i
       @conditions.merge!(price: min..max)
+    end
+
+    def bathrooms
+      bathrooms = params[:bathrooms]
+      return if bathrooms.blank?
+      @conditions.merge!(bathrooms: bathrooms)
+    end
+
+    def bedrooms
+      min = params[:bedrooms][:min].to_i
+      max = params[:bedrooms][:max].blank? ? (1.0 / 0.0) : params[:bedrooms][:max].to_i
+      @conditions.merge!(bedrooms: min..max)
+    end
+
+    def parking
+      parking = params[:parking]
+      return if parking.blank?
+      @conditions.merge!(parking: parking)
     end
 
 end
