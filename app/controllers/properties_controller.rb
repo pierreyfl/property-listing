0a13315514fe0dbd98d809fd459a4603d651e33b
Arash Naveed <arsh.nvd@gmail.com>
2018-06-05 13:48:58 +0500

    Use session to remember filter - experimental.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,7 +3,13 @@ class PropertiesController < ApplicationController
   def index
     @conditions = {}
     set_filters
-    @properties = Property.search("*", page: params[:page], per_page: 5, where: @conditions)
+
+    @fil = get_filters
+
+    # byebug
+    @properties = Property.search("*", page: params[:page], per_page: 2, where: @fil)
+
+    # byebug
   end
 
   private
@@ -18,9 +24,21 @@ class PropertiesController < ApplicationController
         "bedrooms"
       ]
 
+      session[:filters] = {}
+
       params.keys.each do |key|
         method(key.to_sym).call if filters.include?(key)
       end
+    end
+
+    def get_filters
+      return if session[:filters].nil?
+      filters = session[:filters].deep_symbolize_keys
+      filters[:price] = string_to_range(filters[:price])
+      # filters[:bedrooms] = string_to_range(filters[:bedrooms])
+      # filters[:bathrooms] = filters[:bathrooms].to_i
+      # byebug
+      return filters
     end
 
 
@@ -38,11 +56,13 @@ class PropertiesController < ApplicationController
             }
           }
         }
-      @conditions.merge!(location)
+      session[:filters].merge!(location)
     end
 
 
     def near
+      return if params[:near].blank?
+      session[:neartext] = params[:near]
       near = Geocoder.search(params[:near]).first
       location = {
         location: {
@@ -53,40 +73,52 @@ class PropertiesController < ApplicationController
             within: "100mi"
           }
         }
-        @conditions.merge!(location)
+        session[:filters].merge!(location)
     end
 
 
     def price
       min = params[:price][:min].to_i
       max = params[:price][:max].to_i.zero? ? (1.0 / 0.0) : params[:price][:max].to_i
-      @conditions.merge!(price: min..max)
+      session[:filters].merge!(price: min..max)
     end
 
 
     def type
-      @conditions.merge!(type: params[:type].keys)
+      session[:filters]['type'] = params[:type].keys
     end
 
 
     def bedrooms
       min = params[:bedrooms][:min].to_i
-      max = params[:bedrooms][:max].to_i.zero? ? (1.0 / 0.0) : params[:bedrooms][:max].to_i
-      @conditions.merge!(bedrooms: min..max)
+      max = params[:bedrooms][:max].to_i
+      if (min.zero? && max.zero?)
+        session[:filters].delete('bedrooms')
+        return
+      end
+      max = max.zero? ? (1.0 / 0.0) : params[:bedrooms][:max].to_i
+      session[:filters].merge!(bedrooms: min..max)
     end
 
 
     def bathrooms
       bathrooms = params[:bathrooms]
       return if bathrooms.to_i.zero?
-      @conditions.merge!(bathrooms: bathrooms)
+      session[:filters].merge!(bathrooms: bathrooms)
     end
-    
+
 
     def parking
       parking = params[:parking]
       return if parking.to_i.zero?
-      @conditions.merge!(parking: parking)
+      session[:filters].merge!(parking: parking)
+    end
+
+    def string_to_range(value)
+      return if value.nil?
+      return value unless value.class == String
+      range = value.split('..').map{|d| (d=="Infinity") ? 1.0 / 0 : Integer(d)}
+      range[0]..range[1]
     end
 
 end
--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -3,7 +3,7 @@ module PropertiesHelper
   def property_types
     tags = ''
     Property.types.each do |k,v|
-      tags << check_box_tag("type[#{k}]").to_s
+      tags << check_box_tag("type[#{k}]", nil, false).to_s
       tags << label_tag("type[#{k}]",k).to_s
       tags << tag.br
     end
