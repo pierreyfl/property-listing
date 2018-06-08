32342b7b5cc7fdca52fb92d58237f38450d0f925
Pierre Yam <pierreyam@icloud.com>
2018-06-08 13:38:16 +0800

    Run

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,23 @@ ActiveRecord::Schema.define(version: 20180607191058) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.string "favoritable_type", null: false
+    t.integer "favoritable_id", null: false
+    t.string "favoritor_type", null: false
+    t.integer "favoritor_id", null: false
+    t.string "scope", default: "favorite", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blocked"], name: "index_favorites_on_blocked"
+    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id"
+    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
+    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor_type_and_favoritor_id"
+    t.index ["scope"], name: "index_favorites_on_scope"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
