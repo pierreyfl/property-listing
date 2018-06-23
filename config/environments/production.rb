bd38f28adf25daf3e54712323dac592de853f1bc
Arash Naveed <arsh.nvd@gmail.com>
2018-06-23 13:07:04 +0500

    fix a typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = "//d3jco3a4e9vq96.cloudfront.net"
-'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
