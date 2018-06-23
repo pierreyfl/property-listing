e24484ecf85d1067f94d3d4fca03def9f934b31a
Arash Naveed <arsh.nvd@gmail.com>
2018-06-23 15:39:16 +0500

    update seeds, add redis config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-# agent_id = User.create do |u|
-#   u.email                 = Faker::Internet.email
-#   u.fullname              = Faker::Name.name
-#   u.provider              = 'provider'
-#   u.uid                   = Faker::Number.number(10)
-#   u.phone_number          = Faker::PhoneNumber.phone_number
-#   u.description           = Faker::Lorem.sentence
-#   u.pin                   = Faker::Number.number(4)
-#   u.phone_verified        = true
-#   u.password              = '11223344'
-#   u.password_confirmation = '11223344'
-# end.id
-#
-# puts "Agent with id #{agent_id} created!, creating properties..." if agent_id
-#
-# 300.times do |i|
-#   Property.create do |p|
-#     p.agent_id               = agent_id
-#     p.name                   = Faker::Lorem.word
-#     p.type                   = Faker::Number.between(0, 4)
-#     p.bedrooms               = Faker::Number.between(1, 10)
-#     p.bathrooms              = Faker::Number.between(1, 4)
-#     p.parking                = Faker::Number.between(1, 3)
-#     p.description            = Faker::Lorem.sentence
-#     p.address                = Faker::Address.full_address
-#     p.price                  = Faker::Number.between(3990, 9999)
-#     p.area                   = Faker::Number.between(300, 1500)
-#     p.availability           = Faker::Number.between(1, 3)
-#   end
-#   puts "#{i.next} property created..."
-# end
-#
-# Property.reindex
+agent_id = User.create do |u|
+  u.email                 = Faker::Internet.email
+  u.fullname              = Faker::Name.name
+  u.provider              = 'provider'
+  u.uid                   = Faker::Number.number(10)
+  u.phone_number          = Faker::PhoneNumber.phone_number
+  u.description           = Faker::Lorem.sentence
+  u.pin                   = Faker::Number.number(4)
+  u.phone_verified        = true
+  u.password              = '11223344'
+  u.password_confirmation = '11223344'
+end.id
+
+puts "Agent with id #{agent_id} created!, creating properties..." if agent_id
+
+300.times do |i|
+  Property.create do |p|
+    p.agent_id               = agent_id
+    p.name                   = Faker::Lorem.word
+    p.type                   = Faker::Number.between(0, 4)
+    p.bedrooms               = Faker::Number.between(1, 10)
+    p.bathrooms              = Faker::Number.between(1, 4)
+    p.parking                = Faker::Number.between(1, 3)
+    p.description            = Faker::Lorem.sentence
+    p.address                = Faker::Address.full_address
+    p.price                  = Faker::Number.between(3990, 9999)
+    p.area                   = Faker::Number.between(300, 1500)
+    p.availability           = Faker::Number.between(1, 3)
+  end
+  puts "#{i.next} property created..."
+end
+
+Property.reindex
 
 
 5.times do |i|
@@ -60,8 +60,8 @@ Property.reindex
 # ClassfiedList.create(:title => "List 2", :price => 45, :time_length => 11)
 # ClassfiedList.create(:title => "List 3", :price => 55, :time_length => 12)
 # ClassfiedList.create(:title => "List 4", :price => 65, :time_length => 13)
-Amenity.create(:name => 'amenity 1', :available => true, :property_package_id => 3)
-Amenity.create(:name => 'amenity 2', :available => true, :property_package_id => 3)
-Amenity.create(:name => 'amenity 3', :available => true, :property_package_id => 3)
-Amenity.create(:name => 'amenity 4', :available => true, :property_package_id => 3)
-Amenity.create(:name => 'amenity 5', :available => true, :property_package_id => 3)
+# Amenity.create(:name => 'amenity 1', :available => true, :property_package_id => 3)
+# Amenity.create(:name => 'amenity 2', :available => true, :property_package_id => 3)
+# Amenity.create(:name => 'amenity 3', :available => true, :property_package_id => 3)
+# Amenity.create(:name => 'amenity 4', :available => true, :property_package_id => 3)
+# Amenity.create(:name => 'amenity 5', :available => true, :property_package_id => 3)
